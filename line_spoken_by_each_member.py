69b5ec8944c6051fe4d15e1314d3b3b806b155a0
myurtoglu <myurtoglu@gmail.com>
2015-06-23 23:03:33 -0700

    Add initial file opening capability and test if file is opened correctly

--- line_spoken_by_each_member.py
+++ line_spoken_by_each_member.py
@@ -1,9 +1,13 @@
 # Goals:
-#      1. Get the whatsapp txt file
 #      2. Provide the number of line spoken by each member
 
+# Assuming whatsapp.txt is in the same directory
+whatsappFileName = "whatsapp.txt"
 
+chatList = []
 
+with open(whatsappFileName, 'r') as infile:
+    chatList = [line.strip() for line in infile]
 
-
-
+for line in chatList:
+    print line
