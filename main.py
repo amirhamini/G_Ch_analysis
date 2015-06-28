f4281fabfc8ee64612df6f202b737ce72a5e3405
myurtoglu <myurtoglu@gmail.com>
2015-06-27 23:50:47 -0700

    Update main.py to get the third command line argument as the whatsapp chat file name

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from wachat import WAChat
+import os
 
 
 def main():
-    myChat = WAChat("whatsapp3.txt")
+    fileName = "whatsapp.txt" if len(os.sys.argv) < 2 else os.sys.argv[1]
+    myChat = WAChat(fileName)
     print myChat.messageList
 
 if __name__ == "__main__":
