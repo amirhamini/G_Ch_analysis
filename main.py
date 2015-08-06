91aa6e7dbd2b01bfe6a30df9b49eb2ea7cb050d6
amirhamini <aminia1987@gmail.com>
2015-08-05 19:26:22 -0700

    new plot usage

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import os
 def main():
     fileName = "whatsapp.txt" if len(os.sys.argv) < 2 else os.sys.argv[1]
     myChat = WAChat(fileName)
-    myChat.plotMembersSpokenLines("p")
+    myChat.plotMembersSpokenLines(plotTitle="July 2015", fromMonth=7,
+                                  fromYear=2015)
+    # print myChat.getMemberFrequencies()["Nick Zeynal"]
 
 
 if __name__ == "__main__":
