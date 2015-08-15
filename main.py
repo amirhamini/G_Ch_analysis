1d1b2766f2bb0076736d8ede82e1be34d869570d
amirhamini <aminia1987@gmail.com>
2015-08-15 15:40:07 -0700

    adopted for new features

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ import os
 def main():
     fileName = "whatsapp.txt" if len(os.sys.argv) < 2 else os.sys.argv[1]
     myChat = WAChat(fileName)
-    myChat.plotMembersSpokenLines(plotTitle="July 2015", fromMonth=7,
-                                  fromYear=2015)
-    # print myChat.getMemberFrequencies()["Nick Zeynal"]
+    # myChat.plotMembersSpokenLines(plotTitle="July 2015", fromMonth=7,
+                                  # fromYear=2015)
+
+    # print myChat.getMemberMonthlyFrequencies()
+    # names = myChat.getMembers().keys()
+    # for name in names:
+    #     myChat.plotOverHoursMemberSpokenLines(name)
+    # for name in names:
+    #     myChat.plotGivenYearOverMonthFrequencies(name)
+    print myChat.getMembers()
+    print myChat.getMemberGivenYearOverMonthFrequencies('Varahram')
+
 
 
 if __name__ == "__main__":
